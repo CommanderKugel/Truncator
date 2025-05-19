3d2536e65977d149ed1c3d5958c24367229031ed
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-05-19 21:01:09 +0200

    Added explanations in comments

--- Truncator/BoardRep/MakeMove.cs
+++ Truncator/BoardRep/MakeMove.cs
@@ -20,8 +20,14 @@ public unsafe partial struct Pos
 
         ulong block = (blocker ^ (1ul << from)) | (1ul << to);
 
+        // Castling is legal if:
+        // 1. we are not in check and we do not move through/into check
+        // 2. we have the corresponding castling rights
+        // 3. the kings and rooks path is not blocked by other pieces
         if (m.IsCastling)
-        {
+        {   
+            // check if we have the castling rights, the path is not blocked
+            // and if we are currently in check
             if (!HasCastlingRight(Us, from < to) ||
                 (blocker & Castling.GetCastlingBlocker(Us, from < to)) != 0 ||
                  GetCheckers() != 0)
@@ -31,28 +37,35 @@ public unsafe partial struct Pos
 
             var (rookStart, rookEnd) = Castling.GetRookCastlingSquares(Us, from < to);
             block = (block ^ (1ul << rookStart)) | (1ul << rookEnd);
-
             int dir = from < to ? 1 : -1;
 
-            for (int sq=from+dir; sq!=to; sq+=dir)
+            // check if we move through check
+            for (int sq = from + dir; sq != to; sq += dir)
             {
                 if ((AttackerTo(sq, block) & ColorBB[(int)Them]) != 0)
                 {
-                    //Console.WriteLine($"{(Square)sq} is attacked! ({m})");
                     return false;
                 }
             }
 
+            // check if we would end in check
             return (AttackerTo(to, block) & ColorBB[(int)Them]) == 0;
         }
 
         if (m.IsEnPassant)
         {
+            // for en-passant, simply always compute the king-attackers with the
+            // blocker configuratio after the move
             block ^= 1ul << EnPassantSquare;
             ulong victim = 1ul << (Us == Color.White ? to - 8 : to + 8);
             return (AttackerTo(ksq, block) & ColorBB[(int)Them] & ~victim) == 0;
         }
 
+        // pseudolegal moves (normal and promotions) are legal, if we dont leave the king 
+        // in check after making the move. Because Movegeneration makes use of check-masks,
+        // only moves are generated that block slider checks or capture the checking piece,
+        // if needed. Thus, only potential pinned pieces can leave the king in check.
+        // If the king and moving Piece are aligned, a legality check is needed.
         return (pt != PieceType.King && !IsInKingsSliderVision(from)) ||
                (AttackerTo(ksq, block) & ColorBB[(int)Them] & ~(1ul << to)) == 0;
     }
@@ -70,11 +83,13 @@ public unsafe partial struct Pos
         PieceType victimPt = PieceTypeOn(to);
         Debug.Assert(movingPt != PieceType.NONE);
 
+        // make the quiet part of the move
         ColorBB[(int)Us] ^= FromToBB;
         PieceBB[(int)movingPt] ^= FromToBB;
 
         FiftyMoveRule++;
 
+        // if capture: remove the victim
         if (victimPt != PieceType.NONE)
         {
             ColorBB[(int)Them] ^= toBB;
@@ -83,6 +98,7 @@ public unsafe partial struct Pos
             FiftyMoveRule = 0;
         }
 
+        // reset the ep-square
         if (EnPassantSquare != (int)Square.NONE)
         {
             EnPassantSquare = (int)Square.NONE;
@@ -90,13 +106,16 @@ public unsafe partial struct Pos
 
         if (movingPt == PieceType.Pawn)
         {
+            // fifty move rule resets, because pawn-moves are not reversible
             FiftyMoveRule = 0;
 
+            // set ep square after double pawn pushes
             if (Math.Abs(from - to) == 16)
             {
                 EnPassantSquare = to;
             }
 
+            // swap the pawn with the promoted piece
             if (m.IsPromotion)
             {
                 PieceType promoPt = m.PromoType;
@@ -104,6 +123,7 @@ public unsafe partial struct Pos
                 PieceBB[(int)promoPt] ^= toBB;
             }
 
+            // capture the ep-victim, as it is not done like normal captures
             if (m.IsEnPassant)
             {
                 int victim = Us == Color.White ? to - 8 : to + 8;
@@ -112,6 +132,7 @@ public unsafe partial struct Pos
             }
         }
 
+        // move the rooks when castling
         if (m.IsCastling)
         {
             var (rookStart, rookEnd) = Castling.GetRookCastlingSquares(Us, from < to);
@@ -120,14 +141,17 @@ public unsafe partial struct Pos
             ColorBB[(int)Us] ^= rookBB;
         }
 
+        // update castling rights
         CastlingRights &= Castling.modifier[from];
         CastlingRights &= Castling.modifier[to];
 
+        // update the kingsquare
         if (movingPt == PieceType.King)
         {
             KingSquares[(int)Us] = to;
         }
 
+        // swap the side-to-move
         Us = Them;
     }
 
