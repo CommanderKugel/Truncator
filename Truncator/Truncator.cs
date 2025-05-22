09ef4b447493abcf481793f730bdc4b454d73372
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-05-22 13:08:46 +0200

    refactors and commenting

---
+++ Truncator/Truncator.cs
@@ -0,0 +1,40 @@
+﻿
+public static class Truncator
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Utils.InitUtils();
+            Attacks.InitAttacks();
+
+            if (args.Length == 0)
+            {
+                UCI.MainLoop();
+            }
+
+            if (args.Length == 1 && args[0] == "bench")
+            {
+                Bench.runBench();
+            }
+
+            if (args.Length == 1 && args[0] == "perft")
+            {
+                Perft.RunPerft();
+            }
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        finally
+        {
+            Attacks.Dispose();
+            Castling.Dispose();
+            Utils.Dispose();
+        }
+    }
+}
--- Truncator/Uci/uci.cs
+++ Truncator/Uci/uci.cs
@@ -10,11 +10,14 @@ public static partial class UCI
 
     public static void MainLoop()
     {
+
         while (true)
         {
             string command = Console.ReadLine() ?? "quit";
             string[] tokens = command.Split(' ');
 
+            // initial command that signals the engine to use the uci-protocol
+            // Truncator only implements uci, so uci initialization is basically skipped
             if (command == "uci")
             {
                 Console.WriteLine("uciok");
@@ -22,35 +25,83 @@ public static partial class UCI
                 Console.WriteLine("id author CommanderKugel");
             }
 
+            // the 'are you still alive?' check from the match runner. will be answered between 
+            // every uci command.
             else if (command == "isready")
             {
                 Console.WriteLine("readyok");
             }
 
+            // options can be set to different values. most importantly are
+            // - size (mb) of the transposition table
+            // - number of threads used for search
             else if (tokens[0] == "setoption")
             {
-                Debug.Assert(state == UciState.Idle, "do not use setoption when engine is not idle!");
+                if (state != UciState.Idle)
+                {
+                    Console.WriteLine("Only use setoption when Truncator is idle!");
+                    continue;
+                }
                 SetOption(tokens);
             }
 
+            // signal to reset all heuristics that save data between moves
             else if (tokens[0] == "ucinewgame")
             {
                 UciRootPos.Clear();
             }
 
+            // initialization of the position to start searching from.
+            // comes with a list of moves most of the time
             else if (tokens[0] == "position")
             {
                 Position(tokens);
             }
 
+            // command to start searching. comes with subcommands most of the time.
             else if (tokens[0] == "go")
             {
                 Go(tokens);
             }
 
+            // signal to close the programm. dont forget to stop all the threads!
             else if (tokens[0] == "quit")
             {
+                thread.Join();
                 return;
+            }
+
+            // ===== CUSTOM COMMANDS ===== //
+
+            // print the current board to the commandline
+            else if (command == "print")
+            {
+                Utils.print(UciRootPos.p);
+            }
+
+            else if (command == "bench")
+            {
+                Bench.runBench();
+            }
+
+            else if (command == "perft")
+            {
+                if (tokens.Length == 1)
+                {
+                    Perft.RunPerft();
+                }
+
+                else if (tokens.Length == 3)
+                {
+                    try
+                    {
+                        Perft.SplitPerft(tokens[1], int.Parse(tokens[2]));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("An error occured when executing 'perft <fen> <depth>' command!");
+                    }
+                }
             }
 
         }
