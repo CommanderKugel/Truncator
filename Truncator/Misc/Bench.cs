40950fe86c89330b313579851baa53e1d8ccc1ef
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-06-03 21:57:46 +0200

    Bump bench up to depth 10

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
-    public const int BenchDepth = 8;
-    public const long BenchNodes = 30699787;
+    public const int BenchDepth = 10;
+    public const long BenchNodes = 67733893;
 
     public static readonly string[] Positions = {
         "r4rk1/5pb1/3R2p1/p2Q1qBp/8/7P/1P3PP1/2R3K1 w - - 4 29",
