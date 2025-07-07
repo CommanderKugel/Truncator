79cf24ea8ed37ece437a5c13e2b585e7842f0f51
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-07-07 12:02:44 +0200

    NonPawn CorrHist Added stoped stc early to run lts over night -------------------------------------------------- Results of dev vs main (5+0.05, 1t, 64MB, UHO_2024_8mvs_big_+105_+124.pgn): Elo: 5.10 +/- 5.48, nElo: 6.60 +/- 7.08 LOS: 96.62 %, DrawRatio: 37.67 %, PairsRatio: 1.07 Games: 9260, Wins: 3175, Losses: 3039, Draws: 3046, Points: 4698.0 (50.73 %) Ptnml(0-2): [426, 966, 1744, 1034, 460], WL/DD Ratio: 2.33 LLR: 1.57 (-2.25, 2.89) [0.00, 5.00] -------------------------------------------------- Results of dev vs main (20+0.2, 1t, 64MB, UHO_2024_8mvs_big_+105_+124.pgn): Elo: 6.58 +/- 4.76, nElo: 9.09 +/- 6.58 LOS: 99.66 %, DrawRatio: 38.61 %, PairsRatio: 1.06 Games: 10722, Wins: 3354, Losses: 3151, Draws: 4217, Points: 5462.5 (50.95 %) Ptnml(0-2): [338, 1256, 2070, 1259, 438], WL/DD Ratio: 1.43 LLR: 2.93 (-2.25, 2.89) [0.00, 5.00] --------------------------------------------------

--- Truncator/Evaluate/CorrectionHistory.cs
+++ Truncator/Evaluate/CorrectionHistory.cs
@@ -10,13 +10,21 @@ public struct CorrectionHistory : IDisposable
     private const int MAX_BONUS = HistVal.HIST_VAL_MAX / 4;
     private const int MIN_BONUS = -HistVal.HIST_VAL_MAX / 4;
 
+    // MinorTable failed +1 elo stc (yellow :[) 
+    // MajorTable failed -3 elo stc 
     private unsafe HistVal* PawnTable;
+    private unsafe HistVal* WhiteNonPawnTable;
+    private unsafe HistVal* BlackNonPawnTable;
 
 
     public unsafe CorrectionHistory()
     {
         PawnTable = (HistVal*)NativeMemory.AllocZeroed((nuint)sizeof(HistVal) * SIZE * 2);
+        WhiteNonPawnTable = (HistVal*)NativeMemory.AllocZeroed((nuint)sizeof(HistVal) * SIZE * 2);
+        BlackNonPawnTable = (HistVal*)NativeMemory.AllocZeroed((nuint)sizeof(HistVal) * SIZE * 2);
     }
+
+    private ulong MakeKey(Color c, ulong key) => (ulong)c * SIZE + key % SIZE;
 
     /// <summary>
     /// Alter Static Evaluation based on past Search results
@@ -24,10 +32,14 @@ public struct CorrectionHistory : IDisposable
     /// </summary>
     public unsafe int Correct(ref Pos p, Node* n)
     {
-        int PawnVal = 16 * PawnTable[p.PawnKey];
-        int CorrectionValue = PawnVal;
+        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null);
 
+        int CorrectionValue = 0;
+        CorrectionValue += 16 * PawnTable[MakeKey(p.Us, p.PawnKey)];
+        CorrectionValue += 12 * WhiteNonPawnTable[MakeKey(p.Us, p.NonPawnKeys[(int)Color.White])];
+        CorrectionValue += 12 * BlackNonPawnTable[MakeKey(p.Us, p.NonPawnKeys[(int)Color.Black])];
         CorrectionValue /= HistVal.HIST_VAL_MAX;
+
         n->StaticEval = Math.Clamp(n->UncorrectedStaticEval + CorrectionValue, -Search.SCORE_EVAL_MAX, Search.SCORE_EVAL_MAX);
 
         return CorrectionValue;
@@ -39,24 +51,36 @@ public struct CorrectionHistory : IDisposable
     /// </summary>
     public unsafe void Update(ref Pos p, int score, int eval, int depth)
     {
+        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null);
         var delta = Math.Clamp((score - eval) * depth / 8, MIN_BONUS, MAX_BONUS);
 
-        PawnTable[p.PawnKey].Update(delta);
+        PawnTable[MakeKey(p.Us, p.PawnKey)].Update(delta);
+        WhiteNonPawnTable[MakeKey(p.Us, p.OurNonPawnKey)].Update(delta);
+        BlackNonPawnTable[MakeKey(p.Us, p.NonPawnKeys[(int)Color.Black])].Update(delta);
     }
 
-
+    /// <summary>
+    /// Fills all tables with zero
+    /// </summary>
     public unsafe void Clear()
     {
-        Debug.Assert(PawnTable != null);
+        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null);
         NativeMemory.Clear(PawnTable, (nuint)sizeof(HistVal) * SIZE * 2);
+        NativeMemory.Clear(WhiteNonPawnTable, (nuint)sizeof(HistVal) * SIZE * 2);
+        NativeMemory.Clear(BlackNonPawnTable, (nuint)sizeof(HistVal) * SIZE * 2);
     }
 
+    /// <summary>
+    /// Frees all allocated Memory. Always free memory before destroying a Corrhist instance
+    /// </summary>
     public unsafe void Dispose()
     {
         if (PawnTable != null)
         {
             NativeMemory.Free(PawnTable);
-            PawnTable = null;
+            NativeMemory.Free(WhiteNonPawnTable);
+            NativeMemory.Free(BlackNonPawnTable);
+            PawnTable = WhiteNonPawnTable = BlackNonPawnTable = null;
         }
     }
 }
--- Truncator/Uci/uci.cs
+++ Truncator/Uci/uci.cs
@@ -9,6 +9,8 @@ public static partial class UCI
 
     public static void MainLoop()
     {
+        // wake up main thread
+        ThreadPool.MainThread.ply = 0;
 
         while (true)
         {
@@ -19,9 +21,9 @@ public static partial class UCI
             // Truncator only implements uci, so uci initialization is basically skipped
             if (command == "uci")
             {
-                Console.WriteLine("id name Truncator 0.47");
+                Console.WriteLine("id name Truncator 0.48");
                 Console.WriteLine("id author CommanderKugel");
-                
+
                 Console.WriteLine($"option name Hash type spin default {TranspositionTable.DEFAULT_SIZE} min {TranspositionTable.MIN_SIZE} max {TranspositionTable.MAX_SIZE}");
                 Console.WriteLine($"option name Threads type spin default 1 min 1 max {ThreadPool.MAX_THREAD_COUNT}");
                 Console.WriteLine($"option name UCI_ShowWDL type check default false");
@@ -118,7 +120,7 @@ public static partial class UCI
                     }
                 }
             }
-            
+
         }
     }
 
