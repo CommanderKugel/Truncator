98be837a8dd3add4d412fdb72cc37a9096e66d53
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-07-21 17:49:49 +0200

    Add Major Corrhist bench: 11059224

--- Truncator/Evaluate/CorrectionHistory.cs
+++ Truncator/Evaluate/CorrectionHistory.cs
@@ -16,6 +16,7 @@ public struct CorrectionHistory : IDisposable
     private unsafe HistVal* WhiteNonPawnTable;
     private unsafe HistVal* BlackNonPawnTable;
     private unsafe HistVal* MinorTable;
+    private unsafe HistVal* MajorTable;
 
 
     public unsafe CorrectionHistory()
@@ -24,6 +25,7 @@ public struct CorrectionHistory : IDisposable
         WhiteNonPawnTable = (HistVal*)NativeMemory.AllocZeroed((nuint)sizeof(HistVal) * SIZE * 2);
         BlackNonPawnTable = (HistVal*)NativeMemory.AllocZeroed((nuint)sizeof(HistVal) * SIZE * 2);
         MinorTable = (HistVal*)NativeMemory.AllocZeroed((nuint)sizeof(HistVal) * SIZE * 2);
+        MajorTable = (HistVal*)NativeMemory.AllocZeroed((nuint)sizeof(HistVal) * SIZE * 2);
     }
 
     private ulong MakeKey(Color c, ulong key) => (ulong)c * SIZE + key % SIZE;
@@ -34,13 +36,14 @@ public struct CorrectionHistory : IDisposable
     /// </summary>
     public unsafe int Correct(ref Pos p, Node* n)
     {
-        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null && MinorTable != null);
+        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null && MinorTable != null && MajorTable != null);
 
         int CorrectionValue = 0;
         CorrectionValue += 16 * PawnTable[MakeKey(p.Us, p.PawnKey)];
         CorrectionValue += 12 * WhiteNonPawnTable[MakeKey(p.Us, p.NonPawnMaterialKey(Color.White))];
         CorrectionValue += 12 * BlackNonPawnTable[MakeKey(p.Us, p.NonPawnMaterialKey(Color.Black))];
         CorrectionValue += 12 * MinorTable[MakeKey(p.Us, p.MinorKey)];
+        CorrectionValue += 12 * MajorTable[MakeKey(p.Us, p.MajorKey)];
         CorrectionValue /= HistVal.HIST_VAL_MAX;
 
         n->StaticEval = Math.Clamp(n->UncorrectedStaticEval + CorrectionValue, -Search.SCORE_EVAL_MAX, Search.SCORE_EVAL_MAX);
@@ -54,13 +57,14 @@ public struct CorrectionHistory : IDisposable
     /// </summary>
     public unsafe void Update(ref Pos p, int score, int eval, int depth)
     {
-        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null && MinorTable != null);
+        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null && MinorTable != null && MajorTable != null);
         var delta = Math.Clamp((score - eval) * depth / 8, MIN_BONUS, MAX_BONUS);
 
         PawnTable[MakeKey(p.Us, p.PawnKey)].Update(delta);
         WhiteNonPawnTable[MakeKey(p.Us, p.NonPawnMaterialKey(Color.White))].Update(delta);
         BlackNonPawnTable[MakeKey(p.Us, p.NonPawnMaterialKey(Color.Black))].Update(delta);
         MinorTable[MakeKey(p.Us, p.MinorKey)].Update(delta);
+        MajorTable[MakeKey(p.Us, p.MajorKey)].Update(delta);
     }
 
     /// <summary>
@@ -68,11 +72,12 @@ public struct CorrectionHistory : IDisposable
     /// </summary>
     public unsafe void Clear()
     {
-        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null && MinorTable != null);
+        Debug.Assert(PawnTable != null && WhiteNonPawnTable != null && BlackNonPawnTable != null && MinorTable != null && MajorTable != null);
         NativeMemory.Clear(PawnTable, (nuint)sizeof(HistVal) * SIZE * 2);
         NativeMemory.Clear(WhiteNonPawnTable, (nuint)sizeof(HistVal) * SIZE * 2);
         NativeMemory.Clear(BlackNonPawnTable, (nuint)sizeof(HistVal) * SIZE * 2);
         NativeMemory.Clear(MinorTable, (nuint)sizeof(HistVal) * SIZE * 2);
+        NativeMemory.Clear(MajorTable, (nuint)sizeof(HistVal) * SIZE * 2);
     }
 
     /// <summary>
@@ -86,7 +91,9 @@ public struct CorrectionHistory : IDisposable
             NativeMemory.Free(WhiteNonPawnTable);
             NativeMemory.Free(BlackNonPawnTable);
             NativeMemory.Free(MinorTable);
-            PawnTable = WhiteNonPawnTable = BlackNonPawnTable = MinorTable = null;
+            NativeMemory.Free(MajorTable);
+            PawnTable = WhiteNonPawnTable = BlackNonPawnTable =
+            MinorTable = MajorTable = null;
         }
     }
 }
