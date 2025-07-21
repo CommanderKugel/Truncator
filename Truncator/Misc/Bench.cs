cfc49776996253c61888a83690672e9b1985df3f
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-07-21 18:05:36 +0200

    Correct Bench ._. bench: 10515514

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 11188685;
+    public const long BenchNodes = 10515514;
     
 
     public static readonly string[] Positions = {
