1d3b7dfbd025e7002989c3c65272cc2b4d4b529d
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-07-26 10:56:31 +0200

    implement tb-wdl probing in-search ethereals code may have been used as a reference https://github.com/AndyGrant/Ethereal/blob/0e47e9b67f345c75eb965d9fb3e2493b6a11d09a/src/search.c#L416

--- Truncator/Search/SearchValues.cs
+++ Truncator/Search/SearchValues.cs
@@ -1,12 +1,20 @@
 public static partial class Search
 {
+    public const int MAX_PLY = 256;
 
-    public const int SCORE_MATE = 32_000;
+    public const int SCORE_MATE = 32_000 + MAX_PLY;
+    public const int SCORE_MATE_IN_MAX = 32_000;
+
+    public const int SCORE_TB_WIN = 31_000 + MAX_PLY;
+    public const int SCORE_TB_WIN_IN_MAX = 31_000;
+
     public const int SCORE_DRAW = 0;
     public const int SCORE_EVAL_MAX = 30_000;
 
     public static bool IsTerminal(int score) => Math.Abs(score) > SCORE_EVAL_MAX;
     public static bool IsLoss(int score) => score < -SCORE_EVAL_MAX;
+    public static bool IsMate(int score) => Math.Abs(score) >= SCORE_MATE_IN_MAX;
+    public static bool IsTbScore(int score) => Math.Abs(score) >= SCORE_TB_WIN_IN_MAX && !IsMate(score);
 
     public const int NONE_BOUND = 0b00,
                      UPPER_BOUND = 0b01,
