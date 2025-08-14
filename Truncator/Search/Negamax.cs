b1e89a8515b097b766bf3846d32148c28d1f739a
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-08-14 21:40:23 +0200

    add threat inputs to main hist + if from sq is threatened + if to sq is threatened bench: 13863921

--- Truncator/Search/Negamax.cs
+++ Truncator/Search/Negamax.cs
@@ -287,7 +287,7 @@ public static partial class Search
             bool isCapture = p.IsCapture(m);
             bool isNoisy = isCapture || m.IsPromotion; // ToDo: GivesCheck()
 
-            ns->HistScore = isCapture ? 0 : thread.history.Butterfly[p.Us, m];
+            ns->HistScore = isCapture ? 0 : thread.history.Butterfly[p.Threats, p.Us, m];
 
             // move loop pruning
             if (!isRoot
