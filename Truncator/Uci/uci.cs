53f96b5674d5a8aff5f21016ead0067244a868e9
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-06 00:32:51 +0200

    add uci option to set hardnodes bench: 7201117

--- Truncator/Uci/uci.cs
+++ Truncator/Uci/uci.cs
@@ -32,6 +32,8 @@ public static partial class UCI
                 Console.WriteLine($"option name SyzygyPath type string default <empty>");
                 //Console.WriteLine($"option name SyzygyProbePly type spin default 40 min 1 max 128");
 
+                Console.WriteLine($"option name Hardnodes type spin default {long.MaxValue} min {1} max {long.MaxValue}");
+
 #if SPSA
                 SpsaUciOption.CollectOptions();
                 SpsaUciOption.PrintOptionsToUCI();
