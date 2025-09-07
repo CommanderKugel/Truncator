5a0de35bb31314601c64b16d54f64895e6a3d90c
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-07 14:46:18 +0200

    add more spsa values and run ltc tune bench: 7005942

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 7201117;
+    public const long BenchNodes = 7005942;
     
 
     public static readonly string[] Positions = {
