1c36e28e2652a87d2eb42c88b70905041c9f50e6
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-14 01:27:59 +0200

    change net parameter a bit + 20 BS -> 50 SB + 0.2 wdl -> 0.5 wdl + filter <= 16 ply -> filter <= 8 ply bench: 8707074

--- Truncator/Evaluate/NNUE/Weights.cs
+++ Truncator/Evaluate/NNUE/Weights.cs
@@ -18,7 +18,7 @@ public static class Weights
 
     public static unsafe void Load()
     {
-        string name = "quantised";
+        string name = "pesto_virifmt_64hl-50";
 
         Stream? stream;
         if ((stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(
--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 9282165;
+    public const long BenchNodes = 8707074;
     
 
     public static readonly string[] Positions = {
