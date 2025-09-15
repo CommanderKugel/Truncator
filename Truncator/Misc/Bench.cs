53e0abea541195e29eca392cc4c49a6a1311fb78
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-16 01:03:04 +0200

    increase hl size to 96 nodes bench: 7972347

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 7902746;
+    public const long BenchNodes = 7972347;
     
 
     public static readonly string[] Positions = {
