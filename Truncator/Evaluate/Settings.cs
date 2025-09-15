d829585c790538d3fff3a4029d6345d6037ec80f
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-16 00:22:58 +0200

    increase netsize to 128 hl nodes bench: 7902746

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -2,11 +2,11 @@ public static class Settings
 {
 
     public const int IN_SIZE = 768;
-    public const int L2_SIZE = 64;
+    public const int L2_SIZE = 128;
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "64hl";
+    public const string NET_NAME = "128hl";
 
     public const int QA = 255;
     public const int QB = 64;
