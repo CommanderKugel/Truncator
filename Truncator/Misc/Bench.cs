11cf325df604d3cf262e6e74f2319ac0eeb4a207
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-16 23:33:57 +0200

    regenerate dataset and train new net same training settings otherwise bench: 7960262

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 7902746;
+    public const long BenchNodes = 7960262;
     
 
     public static readonly string[] Positions = {
