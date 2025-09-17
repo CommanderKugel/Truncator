faf0adb9cbfad64bb6dc64e4e4f4a84c2dde7758
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-17 07:56:02 +0200

    use 0.5 wdl when training trained on 64hl_v1_1mgames.viriformat bench: 7688767

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -6,7 +6,7 @@ public static class Settings
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "64hl";
+    public const string NET_NAME = "64hl-3";
 
     public const int QA = 255;
     public const int QB = 64;
