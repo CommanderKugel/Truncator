0340ce2ce2275f9e1e527cc7749677b0ab8a0395
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-29 21:20:52 +0200

    use all 64hl training data with adjudication dataset1: 64hl_v5_1001kgames_adjudication.viriformat dataset2: 64hl_v6-999kgames_fm250_adj_nopvmlp.viriformat dataset3: 64hl_v7-984kgames_fm250_adj_spsa.viriformat dataset4: 64hl_v8-1005kgames_fm250_adj_smp4t.viriformat dataset5: 64hl_v9_1000kgames_gm250_adj-regen1.viriformat dataset6: 64hl_v9.1_1001kgames_fm250_adj-regen2.viriformat bench: 7242908

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -8,7 +8,7 @@ public static class Settings
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "64hl-fm250-adj-moredata";
+    public const string NET_NAME = "64hl-fm250-adj-moredata-2";
 
     public const int QA = 255;
     public const int QB = 64;
