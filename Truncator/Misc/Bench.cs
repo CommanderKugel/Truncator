78b0c58c4b9c67e35837c8e88d58319cc380066a
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-09-30 19:14:21 +0200

    spsa tune psqt tables bench: 9525595

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 8365036;
+    public const long BenchNodes = 9525595;
     
 
     public static readonly string[] Positions = {
