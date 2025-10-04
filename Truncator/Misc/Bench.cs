43ae3be8a91d752f4b58d29793c75178a73a9ab0
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-05 01:04:05 +0200

    increase hidden layer size to 512 also increase superbatches from 50 to 100 dataset: 10Mgames big bench: 7493417

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 7619329;
+    public const long BenchNodes = 7493417;
     
 
     public static readonly string[] Positions = {
