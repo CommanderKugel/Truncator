48a76e441c6fa265c26db1320c66a5ac45425519
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-08 22:34:26 +0200

    train new net but 0.25 -> 0.50 wdl same dataset as re-gen net bench: 6828603

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 6861667;
+    public const long BenchNodes = 6828603;
     
 
     public static readonly string[] Positions = {
