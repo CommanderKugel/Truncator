597336b14f2caeb1f74d5a4297f96bb726c4d4eb
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-12 17:31:42 +0200

    bench: 7249584

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 7374248;
+    public const long BenchNodes = 7249584;
     
 
     public static readonly string[] Positions = {
