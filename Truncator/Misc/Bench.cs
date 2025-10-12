4b518a0b5585548ca19522ca3aec6c73cbee85e1
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-12 20:44:10 +0200

    train net with same wdl but lower lr bench: 7063055

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 7374248;
+    public const long BenchNodes = 7063055;
     
 
     public static readonly string[] Positions = {
