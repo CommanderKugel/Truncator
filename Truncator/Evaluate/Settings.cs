0367a237b06c9039282c5bc7e9b9d8d22b38c825
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-16 08:30:37 +0200

    same training schedule but superbatches 100->400 bench: 7337540

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -8,7 +8,7 @@ public static class Settings
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "768hl_hm_8ob_0.5wdl_scheduled";
+    public const string NET_NAME = "768hl_hm_8ob_400sb";
 
     public const int QA = 255;
     public const int QB = 64;
