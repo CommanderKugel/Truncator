d791166877cbc210d00bfd63295ae29bb2a13f6e
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-18 00:31:29 +0200

    decrease end-lr by 1/10 bench: 6699161

--- Truncator/Misc/Bench.cs
+++ Truncator/Misc/Bench.cs
@@ -1,7 +1,7 @@
 public static class Bench
 {
     public const int BenchDepth = 12;
-    public const long BenchNodes = 7084418;
+    public const long BenchNodes = 6699161;
     
 
     public static readonly string[] Positions = {
