49cf99a48cea6a3ac524fc4d3d3d5f88bf145f8e
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-26 22:10:19 +0100

    reduce by 2 in cutnodes bench: 8198593

--- Truncator/Search/Negamax.cs
+++ Truncator/Search/Negamax.cs
@@ -499,6 +499,8 @@ public static partial class Search
 
                     if (ttPV) R--;
 
+                    if (cutnode) R += 2;
+
                     // ToDo: R += isAllnode
                     if ((ns + 1)->CutoffCount > 2) R++;
 
