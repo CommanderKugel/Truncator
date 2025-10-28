7c72b82608c340611a222e014a531a78a51abc54
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-10-28 21:41:30 +0100

    exact same finetune as 'finetune' run a second time bench: 9619024

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -8,7 +8,7 @@ public static class Settings
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "finetune";
+    public const string NET_NAME = "finetune_b";
 
     public const int QA = 255;
     public const int QB = 64;
