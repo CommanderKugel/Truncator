d1953ee0a22b06e24262fa728056c05dc67d241b
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-11-10 12:40:56 +0100

    increase hl size to 1280 use main training schedule bench: 9333756

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -2,13 +2,13 @@ public static class Settings
 {
 
     public const int IN_SIZE = 768;
-    public const int L2_SIZE = 768;
+    public const int L2_SIZE = 1280;
 
     public const int OUTPUT_BUCKETS = 8;
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "768hl_hm_8ob_fasterLrDevcay";
+    public const string NET_NAME = "1280hl-1";
 
     public const int QA = 255;
     public const int QB = 64;
