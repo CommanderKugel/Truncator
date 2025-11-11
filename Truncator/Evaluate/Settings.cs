81c566d3c607785d23cbda7256747bfb3e3c02ea
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-11-11 08:17:43 +0100

    add pretraining stage on relabeled data bench: 10069007

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -8,7 +8,7 @@ public static class Settings
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "3_20ksn_0.5";
+    public const string NET_NAME = "pretrain_relabeled";
 
     public const int QA = 255;
     public const int QB = 64;
