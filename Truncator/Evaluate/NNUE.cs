2d68b97ef918e58b4413e92c0dea027166216809
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-11-19 08:04:14 +0100

    managed to get pairwise to be vectorized! bench: 86347

--- Truncator/Evaluate/NNUE.cs
+++ Truncator/Evaluate/NNUE.cs
@@ -19,11 +19,11 @@ public static class NNUE
         var wacc = p.Us == Color.White ? acc.WhiteAcc : acc.BlackAcc;
         var bacc = p.Us == Color.White ? acc.BlackAcc : acc.WhiteAcc;
 
-        Span<sbyte> l1 = stackalloc sbyte[L1_SIZE];
+        Span<short> l1 = stackalloc short[L1_SIZE];
         Span<float> l2 = stackalloc float[L2_SIZE];
         Span<float> l3 = stackalloc float[L3_SIZE];
 
-        fixed (sbyte* l1ptr = l1)
+        fixed (short* l1ptr = l1)
         fixed (float* l2ptr = l2)
         fixed (float* l3ptr = l3)
         {
@@ -36,26 +36,39 @@ public static class NNUE
     }
 
 
-    public static unsafe void ActivatePairwiseCrelu(sbyte* l1, short* wacc, short* bacc)
+    public static unsafe void ActivatePairwiseCrelu(short* l1, short* wacc, short* bacc)
     {
-        for (int i = 0; i < L1_SIZE / 2; i++)
+        var step = Vector256<short>.Count;
+
+        for (int i = 0; i < L1_SIZE / 2; i += step)
         {
             int j = i + L1_SIZE / 2;
 
-            var w_clamp_1 = Math.Clamp(wacc[i], (short)0, QA);
-            var w_clamp_2 = Math.Clamp(wacc[j], (short)0, QA);
+            var w_vec_1 = Avx.LoadAlignedVector256(&wacc[i]);
+            var w_vec_2 = Avx.LoadAlignedVector256(&wacc[j]);
 
-            var b_clamp_1 = Math.Clamp(bacc[i], (short)0, QA);
-            var b_clamp_2 = Math.Clamp(bacc[j], (short)0, QA);
+            var b_vec_1 = Avx.LoadAlignedVector256(&bacc[i]);
+            var b_vec_2 = Avx.LoadAlignedVector256(&bacc[j]);
 
-            l1[i] = (sbyte)((w_clamp_1 * w_clamp_2) >> SHIFT);
-            l1[j] = (sbyte)((b_clamp_1 * b_clamp_2) >> SHIFT);
+            var w_clamp_1 = Vector256.Clamp(w_vec_1, Vector256<short>.Zero, Vector256.Create(QA));
+            var w_clamp_2 = Vector256.Clamp(w_vec_2, Vector256<short>.Zero, Vector256.Create(QA));
+
+            var b_clamp_1 = Vector256.Clamp(b_vec_1, Vector256<short>.Zero, Vector256.Create(QA));
+            var b_clamp_2 = Vector256.Clamp(b_vec_2, Vector256<short>.Zero, Vector256.Create(QA));
+
+            var w_mul = Avx2.MultiplyHigh(Avx2.ShiftLeftLogical(w_clamp_1, 16 - SHIFT), w_clamp_2);
+            var b_mul = Avx2.MultiplyHigh(Avx2.ShiftLeftLogical(b_clamp_1, 16 - SHIFT), b_clamp_2);
+
+            Avx.Store(&l1[i], w_mul);
+            Avx.Store(&l1[j], b_mul);
         }
     }
 
 
-    public static unsafe void ComputeL2(float* l2, sbyte* l1, int bucket)
+    public static unsafe void ComputeL2(float* l2, short* l1, int bucket)
     {
+        var step = Vector256<float>.Count;
+
         // weights
 
         for (int l2node = 0; l2node < L2_SIZE; l2node++)
@@ -70,38 +83,63 @@ public static class NNUE
         // bias
         // screlu
 
-        for (int i = 0; i < L2_SIZE; i++)
+        for (int i = 0; i < L2_SIZE; i += step)
         {
-            l2[i] = l2[i] * L1_NORM + l1_bias[bucket * L2_SIZE + i];
-            l2[i] = Math.Clamp(l2[i], 0, 1);
-            l2[i] *= l2[i];
+            var l2Vec = Vector256.Load(&l2[i]);
+            var normVec = Vector256.Create(L1_NORM);
+            var biasVec = Vector256.LoadAligned(&l1_bias[bucket * L2_SIZE + i]);
+
+            var fma = Fma.MultiplyAdd(l2Vec, normVec, biasVec);
+            var clamped = Vector256.Clamp(fma, Vector256<float>.Zero, Vector256<float>.One);
+            var squared = Avx.Multiply(clamped, clamped);
+
+            Avx.Store(&l2[i], squared);
         }
     }
 
     public static unsafe float ComputeL3(float* l3, float* l2, int bucket)
     {
+        var step = Vector256<float>.Count;
+
         // weights
+
+        var weightPtr = &l2_weight[bucket * L2_SIZE * L3_SIZE];
 
         for (int l3node = 0; l3node < L3_SIZE; l3node++)
         {
-            for (int l2node = 0; l2node < L2_SIZE; l2node++)
-            {
-                l3[l3node] += l2[l2node] * l2_weight[bucket * L2_SIZE * L3_SIZE + l2node * L3_SIZE + l3node];
-            }
-        }
+            var l3acc = Vector256<float>.Zero;
 
-        float output = l3_bias[bucket];
+            for (int l2node = 0; l2node < L2_SIZE; l2node += step)
+            {
+                var weight = Avx.LoadAlignedVector256(&weightPtr[l3node * L2_SIZE + l2node]);
+                var l2Vec = Avx.LoadVector256(&l2[l2node]);
+                
+                l3acc = Fma.MultiplyAdd(weight, l2Vec, l3acc);
+            }
+
+            l3[l3node] = Vector256.Sum(l3acc);
+        }
 
         // bias
         // screlu
         // out weights
 
-        for (int i = 0; i < L3_SIZE; i++)
+        var outAcc = Vector256<float>.Zero;
+
+        for (int i = 0; i < L3_SIZE; i += step)
         {
-            l3[i] += l2_bias[bucket * L3_SIZE + i];
-            l3[i] = Math.Clamp(l3[i], 0, 1);
-            output += l3[i] * l3[i] * l3_weight[bucket * L3_SIZE + i];
+            var biasVec = Vector256.LoadAligned(&l2_bias[bucket * L3_SIZE + i]);
+            var l3Vec = Vector256.Load(&l3[i]);
+            var l3Weight = Vector256.LoadAligned(&l3_weight[bucket * L3_SIZE + i]);
+
+            var sum = Avx.Add(biasVec, l3Vec);
+            var clamp = Vector256.Clamp(sum, Vector256<float>.Zero, Vector256<float>.One);
+            var square = Avx.Multiply(clamp, clamp);
+            
+            outAcc = Fma.MultiplyAdd(square, l3Weight, outAcc);
         }
+
+        var output = Vector256.Sum(outAcc) + l3_bias[bucket];
 
         return output * EVAL_SCALE;
     }
