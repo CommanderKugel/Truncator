cb3254c6244162b6b28b510e592f8bbbe7f95c89
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-11-20 23:40:48 +0100

    bench: 10025677

--- Truncator/Datagen/GenFens.cs
+++ Truncator/Datagen/GenFens.cs
@@ -16,7 +16,8 @@ public static class GenFens
         string BookPath = args[5];
 
         using var temp = new StreamWriter(File.Open(@"C:\Users\nikol\Desktop\Truncator\temp.txt", FileMode.Append));
-        temp.WriteLine(BookPath);  
+        temp.WriteLine(BookPath);
+        temp.WriteLine(AppContext.BaseDirectory);
 
         bool dfrc = args.Length > 6 && args[6] == "dfrc";
         Castling.UCI_Chess960 = dfrc;
