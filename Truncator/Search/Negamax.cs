a3c9ba75eb8e67c612ffd9a88f400e2cf0dc499d
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-11-20 08:54:38 +0100

    add double extensions bench: 14717913

--- Truncator/Search/Negamax.cs
+++ Truncator/Search/Negamax.cs
@@ -450,13 +450,16 @@ public static partial class Search
 
                 if (singularScore < singularBeta)
                 {
+                    extension = 1;
+
                     if (!isPV && singularScore < singularBeta - SEDoubleMargin)
                     {
-                        extension = 2;
+                        extension++;
                     }
-                    else
+
+                    if (!isPV && singularScore < singularBeta - SEDoubleMargin - 50)
                     {
-                        extension = 1;
+                        extension++;
                     }
                 }
             }
