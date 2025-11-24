2ae6df325653314a2b94d1d3fab2f3f85b3f3e81
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-11-24 08:38:07 +0100

    train 1024hl input bucket net on full training schedule bench: 11117318

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -2,28 +2,28 @@ public static class Settings
 {
 
     public const int IN_SIZE = 768;
-    public const int L2_SIZE = 768;
+    public const int L2_SIZE = 1024;
 
     public const int OUTPUT_BUCKETS = 8;
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "768hl_ib";
+    public const string NET_NAME = "1024_inputbuckets";
 
     public const int QA = 255;
     public const int QB = 64;
 
-    public const int INPUT_BUCKETS = 6;
+    public const int INPUT_BUCKETS = 4;
 
     public static ReadOnlySpan<int> KingBucketsLayout => [
-         0,  1,  2,  3,  6,  7,  8,  9,
-         4,  4,  5,  5, 10, 10, 11, 11,
-         4,  4,  5,  5, 10, 10, 11, 11,
-         4,  4,  5,  5, 10, 10, 11, 11,
-         4,  4,  5,  5, 10, 10, 11, 11,
-         4,  4,  5,  5, 10, 10, 11, 11,
-         4,  4,  5,  5, 10, 10, 11, 11,
-         4,  4,  5,  5, 10, 10, 11, 11,
+        0, 0, 1, 1, 4, 4, 5, 5,
+        2, 2, 2, 2, 6, 6, 6, 6,
+        3, 3, 3, 3, 8, 8, 8, 8,
+        3, 3, 3, 3, 8, 8, 8, 8,
+        3, 3, 3, 3, 8, 8, 8, 8,
+        3, 3, 3, 3, 8, 8, 8, 8,
+        3, 3, 3, 3, 8, 8, 8, 8,
+        3, 3, 3, 3, 8, 8, 8, 8,
     ];
 
 }
