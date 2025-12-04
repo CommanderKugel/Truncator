757a76cc731b03ec97ae649d6cc6e3d03c44c7c0
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-12-04 13:06:50 +0100

    train new net on re-generated dataset uses 16 plies of random moves, weighted by low-depth search score bench: 9642154

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -8,7 +8,7 @@ public static class Settings
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "1024-2-0.5";
+    public const string NET_NAME = "1024_tempData";
 
     public const int QA = 255;
     public const int QB = 64;
