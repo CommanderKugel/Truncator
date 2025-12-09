cf9896f4ad837efbadfc95620198f1fbddd0947c
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-12-09 17:35:07 +0100

    remove 20ksn dataset from s3 and replace with 1024gl_5ksn_5tb bench: 10091987

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -8,7 +8,7 @@ public static class Settings
 
     public const int EVAL_SCALE = 400;
 
-    public const string NET_NAME = "1024_tbcorr";
+    public const string NET_NAME = "remove_20ksn-data";
 
     public const int QA = 255;
     public const int QB = 64;
