cb164f4915ffaad64fedbb56e5628195abc697b8
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-12-16 07:27:52 +0100

    train new multilayer net bench: 10536474

--- Truncator/Evaluate/Settings.cs
+++ Truncator/Evaluate/Settings.cs
@@ -3,15 +3,15 @@ public static class Settings
 
     public const int IN_SIZE = 768;
 
-    public const int L1_SIZE = 768;
-    public const int L2_SIZE = 16;
-    public const int L3_SIZE = 32;
+    public const int L1_SIZE = 1024;
+    public const int L2_SIZE = 8;
+    public const int L3_SIZE = 16;
 
     public const int OUT_BUCKETS = 8;
 
-    public const int EVAL_SCALE = 400;
+    public const int EVAL_SCALE = 700;
 
-    public const string NET_NAME = "multilayer-full";
+    public const string NET_NAME = "1024-8-16_multilayer";
 
     public const short QA = 255;
     public const short QB = 64;
